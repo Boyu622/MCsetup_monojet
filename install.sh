344fba75f6606b9975480aa25b081c0eecd66322
Boyu622 <58946881+Boyu622@users.noreply.github.com>
2020-12-03 18:55:02 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,10 +18,16 @@ echo "install pythia8" >&3
 echo "install collier" >&3
 echo "install ninja" >&3
 echo "install MadAnalysis5" >&3
+
+echo "import model DMsimp_s_spin1" >&3
+echo "generate p p > xd xd~ j [QCD]" >&3
+echo "output" -f >&3
+
 exec 3>&-
 
 ./bin/mg5_aMC install.txt
 
 rm -rf install.txt
+rm -rf MG5_aMC_v2.7.3.tar.gz
 
 echo "done"
