c2ad946a4d57610605f65b02008e5ba01e8c955b
Boyu <gao.1559@buckeyemail.osu.edu>
2020-12-06 21:50:51 -0500

    fix mkanalysis problem

--- rivet_monojet/rivet_launch.py
+++ rivet_monojet/rivet_launch.py
@@ -4,13 +4,15 @@ if len(sys.argv)<2:
   print "please provide directory that contains all hepmc files you want to combine"
   sys.exit()
 
-os.system("mv rivet_monojet.cc sample.cc")
-os.system("mv rivet_monojet.plot sample.plot")
+os.system("cp rivet_monojet.cc %s/sample.cc"%sys.argv[1])
+os.system("cp rivet_monojet.plot %s/sample.plot"%sys.argv[1])
+
+os.chdir(sys.argv[1])
+
 os.system("rivet-mkanalysis rivet_monojet")
 os.system("mv sample.cc rivet_monojet.cc")
 os.system("mv sample.plot rivet_monojet.plot")
 os.system("rivet-buildplugin rivet_monojet.cc")
-os.chdir(sys.argv[1])
 
 filestr = " "
 for filename in os.listdir("."):
@@ -25,21 +27,23 @@ for i, fname in enumerate(files):
   infile = open("./rivet-plots/rivet_monojet/" + fname, "r")
   for j, line in enumerate(infile):
     if j == 14: info.append(line)
-    if i == 0 and "# BEGIN HISTO1D" in line: header = line.split(" ")[3].split("EM1")[0]
+    if i == 0 and "# BEGIN HISTO1D" in line: 
+      header = line.split(" ")[3].split("EM1")[0]
+      merginfo = line.split(" ")[3].split("EM1")[1]
   infile.close()
 outfile = open("./rivet-plots/rivet_monojet/" + "MET.dat", "w")
 outfile.write("# BEGIN PLOT" + "\n")
-outfile.write("LegendOnly=" + header + "MET \n")
+outfile.write("LegendOnly=" + header + "MET" + merginfo + "\n")
 outfile.write("XLabel=MET [GeV]" + "\n")
 outfile.write("LegendAlign=r" + "\n")
 outfile.write("YLabel=Events/GeV" + "\n")
-outfile.write("DrawOnly=" + header + "MET \n")
+outfile.write("DrawOnly=" + header + "MET" + merginfo + "\n")
 outfile.write("Title=MET distribution" + "\n")
 outfile.write("Legend=1" + "\n")
 outfile.write("LogY=1" + "\n")
 outfile.write("# END PLOT" + "\n")
 outfile.write("\n")
-outfile.write("# BEGIN HISTO1D " + header + "MET \n")
+outfile.write("# BEGIN HISTO1D " + header + "MET" + merginfo + "\n")
 outfile.write("ErrorBars=1" + "\n")
 outfile.write("LineColor=red" + "\n")
 outfile.write("LineStyle=solid" + "\n")
@@ -50,4 +54,4 @@ for ele in info:
   outfile.write(ele)
 outfile.write("# END HISTO1D" + "\n")
 
-os.system("make-plots --pdf --png ./rivet-plots/rivet_monojet/MET.dat")
+print "you can make MET histogram by 'make-plots --png ./rivet-plots/rivet_monojet/MET.dat' at %s"%os.getcwd()
